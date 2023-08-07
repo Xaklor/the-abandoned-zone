217d168b38b64078832ea0c52e0cab77f482c08b
Xaklor <noqnfluxr@gmail.com>
2023-08-06 22:47:14 -0600

    made food items edible

---
+++ src/main/java/com/xaklor/util/MiracleCure.java
@@ -0,0 +1,63 @@
+package com.xaklor.util;
+
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class MiracleCure extends Item {
+
+    public MiracleCure(Settings settings) {
+        super(settings);
+        register();
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof ServerPlayerEntity serverPlayerEntity) {
+            Criteria.CONSUME_ITEM.trigger(serverPlayerEntity, stack);
+            serverPlayerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        }
+        if (user instanceof PlayerEntity && !((PlayerEntity)user).getAbilities().creativeMode) {
+            stack.decrement(1);
+        }
+        if (!world.isClient) {
+            user.clearStatusEffects();
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_HEALTH, 1, 1));
+        }
+        return stack;
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 32;
+    }
+
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.EAT;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
+    }
+
+    private void register() {
+        Registry.register(Registries.ITEM, new Identifier("the_abandoned_zone", "miracle_cure"), this);
+    }
+}
